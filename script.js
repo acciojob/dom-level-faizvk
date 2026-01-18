2698602a706e2a7ea50b34796d45645bb71c701e
Faiz Zubair <faizvk14@gmail.com>
2026-01-18 17:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the target element
+let element = document.getElementById("level");
+
+// Initialize level count
+let level = 0;
+
+// Traverse up the DOM tree
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+
+// Display the result
+alert("The level of the element is: " + level);
